5ca7769b782d426e6ef55dd6a298575349b9316f
erlend1202 <erlendkri1202@gmail.com>
2022-11-21 12:07:52 +0100

    merge

--- src/main.py
+++ src/main.py
@@ -5,7 +5,4 @@ if __name__ == "__main__":
 
     # Loading covid dataset
     headers, data = load_covid_data()
-    #print(headers, data)
-    died = np.where(data[0:100,4] == -2208988800000000000, 0, 1)
-    print(data[0:100,4])
-    print(died)
+    Y = data[:,4]
